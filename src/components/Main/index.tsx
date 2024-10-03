dd4d42bcbc152f145d9a9114ba858c3f26d41a97
Michel John <michel.santos@dataside.com.br>
2024-10-03 17:44:55 -0300

    test: first test

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,7 @@
+export function Main() {
+  return (
+    <div>
+      <h1 id="oi">Hello main</h1>
+    </div>
+  )
+}
---
+++ src/components/Main/main.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+
+import { Main } from '.'
+
+describe('Main', () => {
+  it('should render correctly', () => {
+    render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: 'Hello main' })
+    ).toBeInTheDocument()
+  })
+})
