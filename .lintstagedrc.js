9b77096d57617a2d1af8a6ebc523159fbed89806
Michel John <michel.santos@dataside.com.br>
2024-10-03 17:40:44 -0300

    chore: change lint-staged extension file

--- .lintstagedrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // Lint & Prettier TS and JS files
-  '*.{ts,tsx}': (filenames) => [
-    `prettier --write ${filenames.join(' ')}`,
-    `pnpm run lint --fix . ${filenames.join(' --file')}`
-  ]
-}
