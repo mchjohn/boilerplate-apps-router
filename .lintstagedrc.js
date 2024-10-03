166f6b4f1a4edeb58a7474e91e3b8485e83f0f98
Michel John <michel.santos@dataside.com.br>
2024-10-03 17:26:07 -0300

    chore: config test on lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,5 +3,6 @@ module.exports = {
   '*.{ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `pnpm run lint --fix . ${filenames.join(' --file')}`,
+    `pnpm test -- --findRelatedTests ${filenames.join(' ')}`,
   ]
 }
